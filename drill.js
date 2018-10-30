1d523be4b85d2c5a5262493b085a242908309165
seankrummel <sean.krummel@gmail.com>
2018-10-30 10:54:47 -0500

    added min function

--- drill.js
+++ drill.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function max(numbers) {
-  let i = 0;
+  let i = 1;
   let num = numbers[0];
   while (i < numbers.length) {
     if (num < numbers[i]) {
@@ -12,4 +12,17 @@ function max(numbers) {
   return num;
 }
 
+function min(numbers) {
+  let i = 1;
+  let num = numbers[0];
+  while (i < numbers.length) {
+    if (num > numbers[i]) {
+      num = numbers[i];
+    }
+    i++;
+  }
+  return num;
+}
+
 console.log(max([1,2,3,2]));
+console.log(min([2,3,5,4]));
